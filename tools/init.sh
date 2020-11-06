e28cbb5ae359c6ff8e663078d41526b307a6e3e0
Daigo Oue <daigo.oue@gmail.com>
2020-11-06 09:03:26 +0900

    installer

---
+++ tools/init.sh
@@ -0,0 +1,4 @@
+sh ./installer/install_zinit.sh
+sh ./installer/install_dein.sh
+sh ./installer/install_pl.sh
+sh ./installer/install_ricty.sh
--- unlink.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-unlink  ~/.vimrc
-unlink  ~/.gvimrc
-unlink  ~/.bashrc
-unlink  ~/.bash_profile
-unlink  ~/.vim
-unlink  ~/.gitconfig
-unlink  ~/.gitignore_global
-unlink  ~/.latexmkrc
